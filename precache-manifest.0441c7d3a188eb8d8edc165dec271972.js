b66421e2feb818197a7264d2be95a3f2951a7f8d
Greta Gadbois <ggadbois@hmc.edu>
2020-06-17 11:55:46 -0400

    Updates

---
+++ precache-manifest.0441c7d3a188eb8d8edc165dec271972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc43f4a7b8b37cb9f52243700a469bc",
+    "url": "/gem-website/index.html"
+  },
+  {
+    "revision": "c0cdb30d98955c5369cf",
+    "url": "/gem-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67017101d55991b67ba7",
+    "url": "/gem-website/static/js/2.f03ad822.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/gem-website/static/js/2.f03ad822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cdb30d98955c5369cf",
+    "url": "/gem-website/static/js/main.0dcdfa5d.chunk.js"
+  },
+  {
+    "revision": "76aabd18c1ff34f9d74c",
+    "url": "/gem-website/static/js/runtime-main.911cda99.js"
+  }
+]);
