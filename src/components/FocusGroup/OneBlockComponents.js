cc0dacd42629374e05814490a8e53fc3996dd04e
Greta Gadbois <ggadbois@hmc.edu>
2020-06-26 09:06:56 -0400

    modal for remove done

--- src/components/FocusGroup/OneBlockComponents.js
+++ src/components/FocusGroup/OneBlockComponents.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BLOCKS } from '../../constants/blocks'
 import { DropdownButton, Dropdown } from 'react-bootstrap'
-import { Table } from 'react-bootstrap'
+import { Table, Modal, Button } from 'react-bootstrap'
 
 const OneBlockComponentHost = ({
   block = '',
@@ -11,10 +11,49 @@ const OneBlockComponentHost = ({
   blocks,
   focusGroup,
 }) => {
+  const [showModal, setShowModal] = useState(false)
+  const handleToggle = () => setShowModal(!showModal)
+
   let blockLongName = BLOCKS[block] ? BLOCKS[block].name : 'Unscheduled'
 
   const RemoveButton = (handleClickFunc) => (
-    <button onClick={handleClickFunc}>Remove</button>
+    <div>
+      <Button onClick={() => handleToggle()} handleClickFunc={handleClickFunc}>
+        Remove
+      </Button>
+      <Modal
+        show={showModal}
+        size='lg'
+        aria-labelledby='contained-modal-title-vcenter'
+        centered
+        backdrop={true}
+        onHide={handleToggle}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id='contained-modal-title-vcenter'>
+            Are you sure you want to delete this?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            This deletes the talk permanently from the database. You should only
+            do this if it's a repeat or if the presenter made a mistake and will
+            reupload their talk.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => handleToggle()}>No, I don't want to</Button>
+          <Button
+            onClick={() => {
+              handleClickFunc()
+              handleToggle()
+            }}
+          >
+            Yes, delete this talk
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   )
 
   const handleMoveClick = (evt, eventKey) => {
